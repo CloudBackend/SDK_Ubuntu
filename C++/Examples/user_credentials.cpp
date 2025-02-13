5b9f7c5f9641a7e53de19d800cff938e3153d3bb
someawe <dev@weister.net>
2025-02-13 23:03:54 +0100

    Release 2.1.4

--- C++/Examples/user_credentials.cpp
+++ C++/Examples/user_credentials.cpp
@@ -2,9 +2,18 @@
 #define EXAMPLE_CREDENTIALS
 #include <string>
 
+// Replace "..." with your own credentials
+
 std::string username = "githubtester1";
 std::string password = "gitHubTester1password";
+
+// std::string username = "githubtester2";
+// std::string password = "gitHubTester2password";
+
+// std::string username = "githubtester3";
+// std::string password = "gitHubTester3password";
+
 std::string tenant   = "cbe_githubtesters";
 std::string client   = "linux_desktop";
 
-#endif
+#endif  // EXAMPLE_CREDENTIALS
