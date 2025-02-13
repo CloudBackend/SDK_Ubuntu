55b2c47771b23b5be909009ea54f3508075772bb
someawe <dev@weister.net>
2025-02-13 22:41:16 +0100

    Release 2.1.4

--- C++/Examples/ChopSyncExcept/run.sh
+++ C++/Examples/ChopSyncExcept/run.sh
@@ -6,5 +6,5 @@ echo "CloudBackend SDK is provided under a limited evaluation licence."
 echo "Not for production use."
 
 export LD_LIBRARY_PATH="../../lib/Linux_x86"
-././partition_large_csv_file $*
+./partition_large_csv_file $*
 
